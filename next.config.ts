6a42b6bc1943c524e3b620cbfc7f37e1eeb6902d
prashant07kumar <prashant033a@gmail.com>
2026-05-30 17:24:08 +0530

    feat: Enhance configuration files with improved Sass options and TypeScript formatting

--- next.config.ts
+++ next.config.ts
@@ -8,12 +8,13 @@
 
 
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
   sassOptions: {
-    silenceDeprecations: ['legacy-js-api'],
+    silenceDeprecations: ['legacy-js-api', 'color-functions'],
   },
 };
 
