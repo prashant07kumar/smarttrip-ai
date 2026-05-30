ca7d35815ba627f3b05c962581ed9e7cb1e981c7
prashant07kumar <prashant033a@gmail.com>
2026-05-30 17:09:16 +0530

    feat: Update configuration files for improved Sass options and TypeScript paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   trailingSlash: false,
+// };
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api'],
+  },
 };
 
 export default nextConfig;
